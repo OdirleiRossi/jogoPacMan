353c3bdb74cf27b9f8bd0d542f7e533ad9416d6b
Odirlei Rossi <odirleirossi@yahoo.com.br>
2022-10-28 17:13:39 -0300

    Implementado o fantasma vermelho e parcialmente os movimentos

---
+++ lib/fantasma.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyGhost extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('lib/images/fantasmaVermelho.png');
+  }
+}
